478abc1b090a10135eeb552f9c6447193df1c8ee
pranong <nutpat2539@gmail.com>
2022-03-25 00:14:33 +0700

    setting things up in progress

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,65 +24,197 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
+      <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
+      <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      <div v-if="$auth.loggedIn">
+        {{ $auth.user.email }}
+        <v-btn @click="$auth.logout">Logout</v-btn>
+      </div>
+      <div v-else>
+        <v-btn @click="dialog = true">Login</v-btn>
+        <!-- <v-btn @click="$auth.loginWith('local', { params: { /* additional authentication parameters */ } })
+            .then(() => this.$toast.success('Logged In!'))">Login</v-btn> -->
+      </div>
+      <v-dialog v-model="dialog" persistent max-width="600px" min-width="360px">
+        <div>
+          <v-tabs
+            v-model="tab"
+            show-arrows
+            background-color="deep-purple accent-4"
+            icons-and-text
+            dark
+            grow
+          >
+            <v-tabs-slider color="purple darken-4"></v-tabs-slider>
+            <v-tab v-for="(i, index) in tabs" :key="index">
+              <v-icon large>{{ i.icon }}</v-icon>
+              <div class="caption py-1">{{ i.name }}</div>
+            </v-tab>
+            <v-tab-item>
+              <v-card class="px-4">
+                <v-card-text>
+                  <v-form ref="loginForm" v-model="valid" lazy-validation>
+                    <v-row>
+                      <v-col cols="12">
+                        <v-text-field
+                          v-model="login.username"
+                          :rules="loginEmailRules"
+                          label="E-mail"
+                          required
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12">
+                        <v-text-field
+                          v-model="login.password"
+                          :append-icon="show1 ? 'eye' : 'eye-off'"
+                          :rules="[rules.required, rules.min]"
+                          :type="show1 ? 'text' : 'password'"
+                          name="input-10-1"
+                          label="Password"
+                          hint="At least 8 characters"
+                          counter
+                          @click:append="show1 = !show1"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col class="d-flex" cols="12" sm="6" xsm="12"> </v-col>
+                      <v-spacer></v-spacer>
+                      <v-col class="d-flex" cols="12" sm="3" xsm="12" align-end>
+                        <v-btn
+                          x-large
+                          block
+                          :disabled="!valid"
+                          color="success"
+                          @click="userLogin"
+                        >
+                          Login
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-tab-item>
+            <v-tab-item>
+              <v-card class="px-4">
+                <v-card-text>
+                  <v-form ref="registerForm" v-model="valid" lazy-validation>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="6">
+                        <v-text-field
+                          v-model="firstName"
+                          :rules="[rules.required]"
+                          label="First Name"
+                          maxlength="20"
+                          required
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="6">
+                        <v-text-field
+                          v-model="lastName"
+                          :rules="[rules.required]"
+                          label="Last Name"
+                          maxlength="20"
+                          required
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12">
+                        <v-text-field
+                          v-model="email"
+                          :rules="emailRules"
+                          label="E-mail"
+                          required
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12">
+                        <v-text-field
+                          v-model="password"
+                          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                          :rules="[rules.required, rules.min]"
+                          :type="show1 ? 'text' : 'password'"
+                          name="input-10-1"
+                          label="Password"
+                          hint="At least 8 characters"
+                          counter
+                          @click:append="show1 = !show1"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12">
+                        <v-text-field
+                          block
+                          v-model="verify"
+                          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                          :rules="[rules.required, passwordMatch]"
+                          :type="show1 ? 'text' : 'password'"
+                          name="input-10-1"
+                          label="Confirm Password"
+                          counter
+                          @click:append="show1 = !show1"
+                        ></v-text-field>
+                      </v-col>
+                      <v-spacer></v-spacer>
+                      <v-col class="d-flex ml-auto" cols="12" sm="3" xsm="12">
+                        <v-btn
+                          x-large
+                          block
+                          :disabled="!valid"
+                          color="success"
+                          @click="validate"
+                          >Register</v-btn
+                        >
+                      </v-col>
+                    </v-row>
+                  </v-form>
+                </v-card-text>
+              </v-card>
+            </v-tab-item>
+          </v-tabs>
+        </div>
+      </v-dialog>
+      <!-- <v-dialog v-model="dialog" max-width="290">
+        <div>
+          <form @submit.prevent="userLogin">
+            <div>
+              <label>Username</label>
+              <input type="text" v-model="login.username" />
+            </div>
+            <div>
+              <label>Password</label>
+              <input type="text" v-model="login.password" />
+            </div>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </v-dialog> -->
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
       <v-list>
         <v-list-item @click.native="right = !right">
           <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
+            <v-icon light> mdi-repeat </v-icon>
           </v-list-item-action>
           <v-list-item-title>Switch drawer (click me)</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -90,29 +222,91 @@
 
 <script>
 export default {
-  name: 'DefaultLayout',
-  data () {
+  name: "DefaultLayout",
+  data() {
     return {
+      dialog: false,
       clipped: false,
       drawer: false,
       fixed: false,
+      login: {
+        username: "",
+        password: "",
+      },
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/",
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire",
+        },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
-}
+      title: "Vuetify.js",
+      tab: 0,
+      tabs: [
+        { name: "Login", icon: "mdi-account" },
+        { name: "Register", icon: "mdi-account-outline" },
+      ],
+      valid: true,
+
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      verify: "",
+      loginPassword: "",
+      loginEmail: "",
+      loginEmailRules: [
+        (v) => !!v || "Required",
+        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+      ],
+      emailRules: [
+        (v) => !!v || "Required",
+        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+      ],
+
+      show1: false,
+      rules: {
+        required: (value) => !!value || "Required.",
+        min: (v) => (v && v.length >= 8) || "Min 8 characters",
+      },
+    };
+  },
+  computed: {
+    passwordMatch() {
+      return () => this.password === this.verify || "Password must match";
+    },
+  },
+  methods: {
+    async userLogin() {
+      try {
+        // let response = await this.$auth.loginWith("local", {
+        //   data: this.login,
+        // });
+        const response = await this.$axios.$get("http://icanhazip.com");
+        console.log(response);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    validate() {
+      if (this.$refs.loginForm.validate()) {
+        // submit form to server/API here...
+      }
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+  },
+};
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
@@ -6,29 +6,25 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - crazyfinds',
-    title: 'crazyfinds',
+    titleTemplate: "%s - crazyfinds",
+    title: "crazyfinds",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -36,22 +32,50 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/auth-next'
-  ],
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth-next"],
+
+  // router: {
+  //   middleware: ['auth']
+  // },
+
+  axios: {
+    baseURL: "http://localhost:9000/api",
+  },
 
   auth: {
-    // Options
+    strategies: {
+      local: {
+        token: {
+          property: "token",
+          global: true,
+          // required: true,
+          // type: 'Bearer'
+        },
+        user: {
+          property: "user",
+          // autoFetch: true
+        },
+        endpoints: {
+          login: { url: "sessions", method: "post", propertyName: "token" },
+          logout: { url: "/api/auth/logout", method: "post" },
+          user: { url: "/api/auth/user", method: "get" },
+        },
+      },
+      auth0: {
+        domain: "dev-g0nuo62m.us.auth0.com",
+        clientId: "DHNVuDSqmd3kWyXxULx4QAyPvZKw3eBU",
+        // logoutRedirectUri: 'http://localhost:3009',
+      },
+    },
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: true,
       themes: {
@@ -62,13 +86,12 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
